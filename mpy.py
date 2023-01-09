7c4072580863bcbae9f3f54bdbf009937a781044
EMRAN-CYBER-0499 <114092973+emran0499@users.noreply.github.com>
2023-01-09 23:29:25 +0400

    Create mpy.py

---
+++ mpy.py
@@ -0,0 +1,12 @@
+#Tor real Abbu emran
+#Tor mare chudi DEC kor
+#Fuck U 
+#এটা মাথাই রাখবি 
+#তোর রিয়েল আব্বু ইমরান
+#চোরের বাচ্চা 
+#কুততার বাচ্চার শরম নাই
+#চোর মাদারচোদ 
+
+import os
+import cyb
+cyb.login()
